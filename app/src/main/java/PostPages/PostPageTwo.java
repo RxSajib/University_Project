32e5215a4b840949c446c1a0c2ceff75c22438cf
SAJIB ROY <sajibroy206@gmail.com>
2020-07-05 23:21:02 +0600

    2nd

---
+++ app/src/main/java/PostPages/PostPageTwo.java
@@ -0,0 +1,29 @@
+package PostPages;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.stamford.stamfordbloodbank.R;
+
+public class PostPageTwo extends Fragment {
+
+    public PostPageTwo(){
+
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.postpagetwo, container, false);
+
+
+        return view;
+    }
+}
